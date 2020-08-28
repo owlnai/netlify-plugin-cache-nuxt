0c4940d44142f6ecbdd4eb7394e00c2fc4d50ad1
Unai Mengual <20427094+owlnai@users.noreply.github.com>
2020-08-28 04:37:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const getCacheDirs = (constants) => [
   constants.PUBLISH_DIR,
-  path.normalize(`${constants.PUBLISH_DIR}/../.cache`),
+  path.normalize(`${constants.PUBLISH_DIR}/../.nuxt`),
 ];
 
 module.exports = {
@@ -10,25 +10,25 @@ module.exports = {
     // print a helpful message if the publish dir is misconfigured
     if (process.cwd() === constants.PUBLISH_DIR) {
       utils.build.failBuild(
-        `Gatsby sites must publish the public directory, but your site’s publish directory is set to “${constants.PUBLISH_DIR}”. Please set your publish directory to your Gatsby site’s public directory.`,
+        `Nuxt sites must publish the public directory, but your site’s publish directory is set to “${constants.PUBLISH_DIR}”. Please set your publish directory to your Nuxt site’s public directory.`,
       );
     }
 
     const cacheDirs = getCacheDirs(constants);
 
     if (await utils.cache.restore(cacheDirs)) {
-      console.log('Found a Gatsby cache. We’re about to go FAST. ⚡️');
+      console.log('Found a Nuxt cache. We’re about to go FAST. ⚡️');
     } else {
-      console.log('No Gatsby cache found. Building fresh.');
+      console.log('No Nuxt cache found. Building fresh.');
     }
   },
   async onPostBuild({ constants, utils }) {
     const cacheDirs = getCacheDirs(constants);
 
     if (await utils.cache.save(cacheDirs)) {
-      console.log('Stored the Gatsby cache to speed up future builds.');
+      console.log('Stored the Nuxt cache to speed up future builds.');
     } else {
-      console.log('No Gatsby build found.');
+      console.log('No Nuxt build found.');
     }
   },
 };
